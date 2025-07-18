ece6386ea85af3889a719a131bccfc30c19cbaa8
Johnogbe <151523621+Johnogbe@users.noreply.github.com>
2025-07-18 16:06:54 +0100

    Update payment.jsx

--- muskcapital/src/pages/payment.jsx
+++ muskcapital/src/pages/payment.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import '../assets/styles/payment.css';
+
 
 import MobileNavbar from '../components/mobile-nav';
 import DesktopNav from '../components/desktop';
