ebab97db06dfb02586e85e1a476f827f8a1c386d
Johnogbe <151523621+Johnogbe@users.noreply.github.com>
2025-07-18 15:33:30 +0100

    Update portfolio.jsx

--- muskcapital/src/pages/portfolio.jsx
+++ muskcapital/src/pages/portfolio.jsx
@@ -95,7 +95,7 @@ setTotalValue(total);
                   <path d="M216,72H56a8,8,0,0,1,0-16H192a8,8,0,0,0,0-16H56A24,24,0,0,0,32,64V192a24,24,0,0,0,24,24H216a16,16,0,0,0,16-16V88A16,16,0,0,0,216,72Zm0,128H56a8,8,0,0,1-8-8V86.63A23.84,23.84,0,0,0,56,88H216Zm-48-60a12,12,0,1,1,12,12A12,12,0,0,1,168,140Z" />
                 </svg>
               ),
-              title:  ${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })},
+              title: `$${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })}`,
               subtitle: "Available to trade"
             },
             {
@@ -104,7 +104,7 @@ setTotalValue(total);
                   <path d="M152,120H136V56h8a32,32,0,0,1,32,32,8,8,0,0,0,16,0,48.05,48.05,0,0,0-48-48h-8V24a8,8,0,0,0-16,0V40h-8a48,48,0,0,0,0,96h8v64H104a32,32,0,0,1-32-32,8,8,0,0,0-16,0,48.05,48.05,0,0,0,48,48h16v16a8,8,0,0,0,16,0V216h16a48,48,0,0,0,0-96Zm-40,0a32,32,0,0,1,0-64h8v64Zm40,80H136V136h16a32,32,0,0,1,0,64Z" />
                 </svg>
               ),
-              title:  ${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })},
+title: `$${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })}`,
               subtitle: "Buying Power"
             },
             {
@@ -122,7 +122,7 @@ setTotalValue(total);
                   <path d="M152,120H136V56h8a32,32,0,0,1,32,32,8,8,0,0,0,16,0,48.05,48.05,0,0,0-48-48h-8V24a8,8,0,0,0-16,0V40h-8a48,48,0,0,0,0,96h8v64H104a32,32,0,0,1-32-32,8,8,0,0,0-16,0,48.05,48.05,0,0,0,48,48h16v16a8,8,0,0,0,16,0V216h16a48,48,0,0,0,0-96Zm-40,0a32,32,0,0,1,0-64h8v64Zm40,80H136V136h16a32,32,0,0,1,0,64Z" />
                 </svg>
               ),
-              title:  ${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })},
+              title: `$${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })}`,
               subtitle: "Cash Balance"
             },
             {
@@ -131,7 +131,7 @@ setTotalValue(total);
                   <path d="M152,120H136V56h8a32,32,0,0,1,32,32,8,8,0,0,0,16,0,48.05,48.05,0,0,0-48-48h-8V24a8,8,0,0,0-16,0V40h-8a48,48,0,0,0,0,96h8v64H104a32,32,0,0,1-32-32,8,8,0,0,0-16,0,48.05,48.05,0,0,0,48,48h16v16a8,8,0,0,0,16,0V216h16a48,48,0,0,0,0-96Zm-40,0a32,32,0,0,1,0-64h8v64Zm40,80H136V136h16a32,32,0,0,1,0,64Z" />
                 </svg>
               ),
-              title:  ${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })},
+              title: `$${totalValue.toLocaleString(undefined, { minimumFractionDigits: 2 })}`,
               subtitle: "Settled Funds"
             }
           ].map((item, index) => (
