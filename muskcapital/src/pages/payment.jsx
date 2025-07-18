e386732f425dda7eaddedc27e757f32263093bca
Johnogbe <151523621+Johnogbe@users.noreply.github.com>
2025-07-18 15:58:01 +0100

    Update payment.jsx

--- muskcapital/src/pages/payment.jsx
+++ muskcapital/src/pages/payment.jsx
@@ -1,10 +1,9 @@
-import React, { useState, } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../assets/styles/payment.css';
-  import { useLocation } from 'react-router-dom';
-  import { useEffect } from 'react';
+
 import MobileNavbar from '../components/mobile-nav';
 import DesktopNav from '../components/desktop';
-import { useNavigate } from 'react-router-dom';
 const CryptoPayment = () => {
   const [selectedCrypto, setSelectedCrypto] = useState('');
   const [isBlurred, setIsBlurred] = useState(true);
