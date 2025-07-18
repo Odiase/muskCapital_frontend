85bbaff4b631ff99f97da9cb124d57f6fe44191a
Johnogbe <151523621+Johnogbe@users.noreply.github.com>
2025-07-18 16:55:20 +0100

    Update mobile-nav.jsx

--- muskcapital/src/components/mobile-nav.jsx
+++ muskcapital/src/components/mobile-nav.jsx
@@ -43,9 +43,7 @@ const MobileNavbar = ({style}) => {
              <li className="mobile-menu-item">
               <Link to="/neuralink" onClick={toggleMenu}>Neuralink</Link>
             </li>
-            <li className="mobile-menu-item">
-              <Link to="/portfolio" onClick={toggleMenu}>Portfolio</Link>
-            </li>
+           
             <li className="mobile-menu-item">
               <button className="mobile-login-btn">
                 <Link to="/login" onClick={toggleMenu}>Login</Link>
